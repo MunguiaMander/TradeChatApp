c3fe6351f354385af60cf16dc8ecd0068848c5aa
Marco Munguia <79735511+MunguiaMander@users.noreply.github.com>
2024-09-05 12:20:10 -0600

    Change the config.php listen port and database conn to 3306 (mysql and xampp default)

--- project/controller/php/config.php
+++ project/controller/php/config.php
@@ -1,10 +1,10 @@
 <?php
-    $hostname = "localhost:3308";
+    $hostname = "localhost:3306";
     $username = "root";
     $password = "";
     $dbname = "tradechatapp";
 
-    // Crear la conexión
+    
     $conn = mysqli_connect($hostname, $username, $password, $dbname);
 
 ?>
